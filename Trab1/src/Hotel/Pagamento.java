f65b6b8e3760b218067d6ad65c1a49bcc60fe5e2
andre <andre@DESKTOP-CFA9KID>
2023-06-04 18:07:38 -0300

    implementação da classe pagamento

---
+++ Trab1/src/Hotel/Pagamento.java
@@ -0,0 +1,41 @@
+package Hotel;
+
+import java.util.Date;
+
+public class Pagamento {
+	private final ETipoPagamento tipo;
+	private final Date data;
+	private final double valor;
+
+	public Pagamento(ETipoPagamento tipo, double valor) {
+		this.data = new Date();
+		this.tipo = tipo;
+		this.valor = valor;
+	}
+
+	public ETipoPagamento getTipo() {
+		return tipo;
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public double getValor() {
+		return valor;
+	}
+
+	public enum ETipoPagamento {
+		PIX("pagamento pix"), DEBITO("pagamento debito"), CREDITO("pagamento credito");
+
+		final String descricao;
+
+		ETipoPagamento(String descricao) {
+			this.descricao = descricao;
+		}
+
+		public String getDescricao() {
+			return descricao;
+		}
+	}
+}
