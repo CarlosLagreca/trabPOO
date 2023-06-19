a2f9cf005253e235a011a9a3f8f0e934eacd3680
Cj <Cj@CJPC>
2023-06-18 21:57:37 -0300

    Implementado TipoAcomodação
    Foi criada a view e a combobox do tipoAcomodação na criação de nova
    Acomodação está funcionando 🥳

--- Trab1/src/controller/AptController.java
+++ Trab1/src/controller/AptController.java
@@ -3,6 +3,8 @@ package controller;
 import java.io.Serializable;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.ArrayList;
+import java.util.List;
 
 import model.Acomodacao;
 import model.Acomodacao.EEstadoOcupacao;
@@ -12,14 +14,19 @@ public class AptController implements Serializable {
 
 	private static final long serialVersionUID = 2252831249496233980L;
 
+	
+	//TODO: Arrumar relação entre acomodação e tipos.
 	private Map<Integer, Acomodacao> apartamentos;
+	private Map<String, TipoAcomodacao> tipos;
 
 	public AptController() {
 		apartamentos = new TreeMap<>();
+		tipos = new TreeMap<>();
 	}
 
-	public void addApartamento(int numero, int ocupacaomax, String tipo) {
-		Acomodacao apartamento = new Acomodacao(numero, ocupacaomax, new TipoAcomodacao(tipo, 10, 1));
+	public void addApartamento(int numero, int ocupacaomax, String nomeTipo) {
+		TipoAcomodacao tipo = tipos.get(nomeTipo);
+		Acomodacao apartamento = new Acomodacao(numero, ocupacaomax, tipo);
 		apartamentos.put(apartamento.getNumero(), apartamento);
 
 		MainController.save();
@@ -32,6 +39,7 @@ public class AptController implements Serializable {
 	// TODO: .MANUTENCAO está aqui por motivos de testes. Tem que tirar.
 	public void concluirLimpeza(int numero) {
 		apartamentos.get(numero).setEstadoOcupacao(EEstadoOcupacao.MANUTENCAO);
+		MainController.save();
 	}
 
 	public int deleteApartamento(int numero) {
@@ -53,5 +61,29 @@ public class AptController implements Serializable {
 		MainController.save();
 
 		return 0;
+	}
+	
+	public void addTipoAcomodacao(String nome, double tarifaDiaria, double adicionalAcompanhante) {
+		TipoAcomodacao novoTipo = new TipoAcomodacao(nome, tarifaDiaria, adicionalAcompanhante);
+		tipos.put(nome, novoTipo);
+		MainController.save();
+	}
+	
+	public String[][] getTipos() {
+		List<String[]> lista = new ArrayList<String[]>();
+		
+		List<String> nomes = new ArrayList<String>();
+		for(String nome : tipos.keySet()) {
+			nomes.add(nome);
+		}
+		lista.add(nomes.toArray(new String[0]));
+		
+		for(String nome : tipos.keySet()) {
+			double addAcomp = tipos.get(nome).getAdicionalAcompanhante();
+			double tarifaD = tipos.get(nome).getTarifaDiaria();
+			String[] infos = {Double.toString(tarifaD), Double.toString(addAcomp)};
+			lista.add(infos);
+		}
+		return lista.toArray(new String[0][0]);
 	}
 }
